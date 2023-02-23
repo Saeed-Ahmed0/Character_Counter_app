eb18f2c5afeac32543a8ae3c6db92a5f9e2ab387
Saeed Ahmed <115498265+Saeed-Ahmed0@users.noreply.github.com>
2023-02-24 00:15:55 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+const charval = document.getElementById("textarea");
+let totalCount = document.getElementById("total-conter");
+let remainingCoutn = document.getElementById("remaining-counter");
+
+let userChar = 0;
+
+// to update the character on screen
+const updateCounter = () => {
+  userChar = charval.value.length;
+
+  totalCount.innerText = userChar;
+
+  remainingCoutn.innerText = 150 - userChar;
+};
+
+charval.addEventListener("keyup", () => updateCounter());
+
+// to copy the text
+const copyText = () => {
+  charval.select();
+  charval.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(charval.value);
+};
+
